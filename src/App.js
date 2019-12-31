2c00ba032849526aa554c7318af1eb97138994bf
yogesh-hackx <yogesh.hackx@gmail.com>
2019-12-31 17:08:39 +0530

    Added functions for creating and joining channel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
 import Cells from './Components/Cells'
+import PubNubReact from 'pubnub-react'
+import shortid from 'shortid'
+import Swal from "sweetalert2"
+import Credentials from './Credentials'
 import './App.css';
 
 function App() {
+
+  let pubnub = new PubNubReact({
+    publishKey: Credentials.publishKey,
+    subscribeKey: Credentials.subscribeKey
+  })
 
   const [state, setState] = useState({
     piece: '',
@@ -17,11 +26,82 @@ function App() {
   let roomId = null
 
   const onPressCreate = (event) => {
+    roomId = shortid.generate().substring(0, 3)
+    lobbyChannel = 'noughtsandcrosses-' + roomId
 
+    pubnub.subscribe({
+      channels: [lobbyChannel],
+      withPresence: true
+    })
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'success',
+      title: roomId,
+      text: 'Share this RoomID with your friend',
+      width: 275,
+    })
+
+    setState({
+      piece: 'X',
+      isPlaying: state.isPlaying,
+      isRoomCreater: true,
+      isDisabled: true,
+      myTurn: true,
+    })
   }
 
   const onPressJoin = (event) => {
-    
+    Swal.fire({
+      input: 'text',
+      allowOutsideClick: false,
+      inputPlaceholder: 'Enter Room ID',
+      showCancelButton: true
+    }).then((result) => {
+      if ( result.value ) {
+        joinRoom(result.value)
+      }
+    })
+  }
+
+  const joinRoom = (value) => {
+    roomId = value
+    lobbyChannel = 'noughtsandcrosses-' + roomId
+
+    pubnub.hereNow({
+      channels: [lobbyChannel]
+    }).then((response) => {
+        if ( response.totalOccupancy <2) {
+            pubnub.subscribe({
+              channels: [lobbyChannel],
+              withPresence: true
+            })
+
+          setState({
+            piece: 'O',
+            isPlaying: state.isPlaying,
+            isRoomCreater: state.isRoomCreater,
+            isDisabled: state.isDisabled,
+            myTurn: state.myTurn,
+          })
+
+          pubnub.publish({
+            message: {
+              notRoomCreater: true,
+            },
+            channel: lobbyChannel
+          })
+        }
+        else {
+          Swal.fire({
+              title: 'Error',
+              text: 'Room Currently Full.',
+              icon: 'error',
+          })
+        }
+    }).catch((error) => {
+      console.log(error);
+    })
   }
 
 
