5716f07c07d148bc5c3d3013398a522fbefb65d6
yogesh-hackx <yogesh.hackx@gmail.com>
2019-12-31 15:54:22 +0530

    Added Room Create and Join Buttons and improved UI

--- src/App.js
+++ src/App.js
@@ -4,16 +4,62 @@ import './App.css';
 
 function App() {
 
-  // const [state, setState] = useState({
-  //   piece: '',
-  // })
+  const [state, setState] = useState({
+    piece: '',
+    isPlaying: false,
+    isRoomCreater: false,
+    isDisabled: false,
+    myTurn: false,
+  })
+
+  let lobbyChannel = null
+  let gameChannel = null
+  let roomId = null
+
+  const onPressCreate = (event) => {
+
+  }
+
+  const onPressJoin = (event) => {
+    
+  }
+
 
   return (
     <div className="App">
-    <h1>Noughts & Crosses</h1>
+    <h1 className="title">Noughts & Crosses</h1>
 
-    <Cells></Cells>
-      
+    {
+      !state.isPlaying &&
+      <div className="game">
+        <div className="cells">
+          <Cells 
+            cell={0}
+            onClick={index => null}
+          />
+        </div>
+
+        <div className="button-container">
+          <button
+            className="create-button btn-primary"
+            disabled={state.isDisabled}
+            onClick={(event) => onPressCreate()}
+            >
+            Create
+          </button>
+          <button className="join-button btn-success"
+            onClick={(event) => onPressJoin()}
+            >
+            Join
+          </button>
+        </div>
+      </div>
+    }
+
+    {
+      // Game Running Initiate
+
+    }
     </div>
   );
 }
