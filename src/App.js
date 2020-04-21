d4a4b08a75bc3373dd46c90a9f67b1d7ba6bb3a3
yogesh-hackx <yogesh.hackx@gmail.com>
2020-04-22 03:10:31 +0530

    Changed GeoLocation API

--- src/App.js
+++ src/App.js
@@ -11,17 +11,13 @@ class App extends React.Component {
 
     success = (position) => {
         this.setState({ lat: position.coords.latitude, long: position.coords.longitude})
-        fetch(`https://trueway-geocoding.p.rapidapi.com/ReverseGeocode?language=en&location=${this.state.lat},${this.state.long}`, {
-        "method": "GET",
-        "headers": {
-                "x-rapidapi-host": "trueway-geocoding.p.rapidapi.com",
-                "x-rapidapi-key": "bc38a9bceemsh82feeb0dbc1960bp128f7fjsnfc5eba2cc5ed"
-        }
+        fetch(`http://www.geoplugin.net/extras/location.gp?lat=${this.state.lat}&lon=${this.state.long}&format=json`, {
+        "method": "GET"
 })
 .then(response => {
     console.log(response.json().then(resp => { 
-        console.log(resp.results[0].region);
-        this.setState({ state: resp.results[0].region, address: resp.results[0].address })
+        console.log(resp.geoplugin_region);
+        this.setState({ state: resp.geoplugin_region })
     }));
 })
 .catch(err => {
@@ -38,8 +34,8 @@ class App extends React.Component {
             <div className="container">
                 <button className="btn btn-primary" onClick={this.fetchState}>Click to get your location:</button>
                 {
-                    this.state.address ? (<div class="alert alert-success" role="alert"><p>Address: {this.state.address}</p><p>Coordinates: {`Lat: ${this.state.lat}, Long: ${this.state.long}`}</p></div>) : 
-                    this.state.state ? <div class="alert alert-success" role="alert"><p>State: {this.state.state}</p><p>Coordinates: {`Lat: ${this.state.lat}, Long: ${this.state.long}`}</p><div class="alert alert-danger" role="alert">Sorry, your complete address is not available at the moment</div></div>
+                    this.state.address ? (<div className="alert alert-success" role="alert"><p>Address: {this.state.address}</p><p>Coordinates: {`Lat: ${this.state.lat}, Long: ${this.state.long}`}</p></div>) : 
+                    this.state.state ? <div className="alert alert-success" role="alert"><p>State: {this.state.state}</p><p>Coordinates: {`Lat: ${this.state.lat}, Long: ${this.state.long}`}</p></div>
                     : null
                 }
 
